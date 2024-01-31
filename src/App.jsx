92f061e77a917674ad677d81a731c8b2e83f136b
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-31 11:51:54 +0700

    [guide] basic req 1: UI markup

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
       style={{ maxWidth: 400, margin: "auto" }}
     >
       Hello
+      {/* 1. render a <h1> with a title */}
+      {/* 2. render a <button> for creating a new account */}
+      {/* 3. render a list of accounts with hardcoded data
+         💡 TIP: you can use `[...Array(10)]` to create array of 10 items
+       */}
     </div>
   );
 }
